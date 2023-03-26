b99e069a6bfbb09fa03c5109dbefc9dd7dc4c91d
ompradeep123 <125042953+ompradeep123@users.noreply.github.com>
2023-03-26 18:05:26 +0530

    Update mycoin.sol

--- mycoin.sol
+++ mycoin.sol
@@ -3,18 +3,13 @@
 pragma solidity ^0.8.0;
 
 contract MyCoin {
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
+    string public name = "META";
+    string public symbol = "MTA";
+    uint256 public totalSupply = 0;
     
     mapping(address => uint256) public balances;
     
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _totalSupply;
-        balances[msg.sender] = _totalSupply;
-    }
+  
     
     function mint(address _to, uint256 _value) public {
         totalSupply += _value;
